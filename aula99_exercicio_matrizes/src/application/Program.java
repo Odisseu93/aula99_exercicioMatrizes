c537ac0d768b9440d61c49ce6f550a3b1adcdf7f
uliss <uliss@ULISSES-HOME-PC>
2021-11-21 09:40:48 -0300

    first upload - Criacao da matriz com o tamanho definido pelo o usuário.

--- aula99_exercicio_matrizes/src/application/Program.java
+++ aula99_exercicio_matrizes/src/application/Program.java
@@ -2,9 +2,7 @@ package application;
 
 import java.util.Scanner;
 
-/*
-Fazer um programa para ler um número inteiro N e uma matriz de ordem N contendo números inteiros.
-Em seguida, mostrar a diagonal principal e a quantidade de números negativos da matriz.*/
+
 public class Program {
 
 	public static void main(String[] args) {
